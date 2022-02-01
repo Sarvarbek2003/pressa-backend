696564447d5100b1b9b0c370984701640ccc1931
Sarvarbek <sarvarbekodilov070603@gmail.com>
2022-02-01 05:14:59 +0500

    test

--- src/routes/announcements.js
+++ src/routes/announcements.js
@@ -2,7 +2,7 @@ const controller = require('../controllers/announcements.js')
 const token = require('../middlewares/token.js')
 const router = require('express').Router()
 
-router.get('/', controller.GET)
+router.get('/',token, controller.GET)
 router.get('/:postId', controller.GET)
 
 module.exports = router
